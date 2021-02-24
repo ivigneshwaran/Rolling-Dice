5ebe34c85db73fa604f01b2df7dba4ce7ea780b0
Vignesh waran <vw170996@gmail.com>
2021-02-24 16:29:27 +0530

    Final op

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,8 @@ export default class Die extends Component {
   render() {
     return (
       <div className="Die">
-        <i className={`Die-icon fas fa-dice-${this.props.face}`}></i>
+        <i className={`Die-icon fas fa-dice-${this.props.face} animate__animated 
+          ${this.props.roll && 'animate__shakeX'}`}></i>
       </div>
     )
   }
--- src/RollDice.js
+++ src/RollDice.js
@@ -11,7 +11,8 @@ export default class RollDice extends Component {
 
     this.state = {
       die1: 'one',
-      die2: 'one'
+      die2: 'one',
+      isRolling: false
     }
     this.roll = this.roll.bind(this);
   }
@@ -21,7 +22,12 @@ export default class RollDice extends Component {
     const rolled1 =  this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     const rolled2 =  this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     //set new state
-    this.setState({die1: rolled1, die2: rolled2})
+    this.setState({die1: rolled1, die2: rolled2, isRolling: true})
+    // wait one sec then change back to roll dice
+    setTimeout(() => {
+      this.setState({isRolling: false})
+    }, 1000);
+
     // animate__shakeX
     // $("Die").addClass("animate__shakeX");
   }
@@ -30,10 +36,11 @@ export default class RollDice extends Component {
     return (
       <div className="RollDice">
         <div className="RollDice-container">
-          <Die face={this.state.die1} />
-          <Die face={this.state.die2} />
+          <Die face={this.state.die1} roll={this.state.isRolling} />
+          <Die face={this.state.die2} roll={this.state.isRolling} />
         </div>
-        <button onClick={this.roll}>Roll Dice!</button>
+        <button onClick={this.roll} disabled={this.state.isRolling}>
+          {this.state.isRolling ? "Rolling...":"Roll Dice!"}</button>
       </div>
     )
   }
